c1b829e9ac7cad4c2cb4cc606f0244b31a6d36f0
optimalprimate <45762167+optimalprimate@users.noreply.github.com>
2019-08-27 14:50:13 +0100

    Update MQQT_template.ino

--- MQQT_template.ino
+++ MQQT_template.ino
@@ -7,15 +7,16 @@ const char* mqttServer = "192.168.19***";
 const int mqttPort = 1883;
 const char* mqttUser = "";
 const char* mqttPassword = "";
-int j = 0;
+//millis for delayed looping
+int delay_period = 5000; //every 5seconds
+unsigned long time_now = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup(void) 
+void setup() 
 { 
  Serial.begin(9600); 
-
 WiFi.mode(WIFI_STA);
 WiFi.begin(ssid, password);
  
@@ -48,15 +49,33 @@ WiFi.begin(ssid, password);
   client.publish("esp/test", "Hello from ESP8266");
 }
 
+//reconnect loop for MQTT Dropout
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    // if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
  
-void loop(void) 
-{ 
-// if (j > 800000) { 
- client.publish("esp/pm1", String("heartbeat").c_str());
-// j=0;
-// }  
+void loop() { 
+  if (!client.connected()) {
+    reconnect();
+  }
 
- delay(300);
-//j = j+1;
-   
+ if(millis() > time_now + delay_period){
+        time_now = millis();
+        client.publish("esp/topic", String("value").c_str());
+    }
 } 
